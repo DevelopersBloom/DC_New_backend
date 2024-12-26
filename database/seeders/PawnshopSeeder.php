a8aadf133fc64b8b582916441c664b8d3f70227b
Ani Mashinyan <ani.mashinyan00@gmail.com>
2024-12-26 22:21:46 +0400

    Change yerevan pawnshop id to 2 in seeder

--- database/seeders/PawnshopSeeder.php
+++ database/seeders/PawnshopSeeder.php
@@ -17,27 +17,6 @@ class PawnshopSeeder extends Seeder
     public function run()
     {
         $pawnshop = new Pawnshop();
-        $pawnshop->city = 'Երևան';
-        $pawnshop->address = 'Արշակունյաց պ. 53/35';
-        $pawnshop->license = 'ԳԳ-Ե 302';
-        $pawnshop->representative = 'Վ. Սահակյան';
-        $pawnshop->telephone = '(010) 49-07-77';
-        $pawnshop->phone1 = '096 99 91 80';
-        $pawnshop->phone2 = '094 97 19 51';
-        $pawnshop->email = 'diamondcredit1@mail.ru';
-        $pawnshop->bank = 'Ամերիաբանկ';
-        $pawnshop->card_account_number = '1570073548790300';
-        $pawnshop->worth = '0';
-        $pawnshop->given = '0';
-        $pawnshop->cashbox = '5000000';
-        $pawnshop->bank_cashbox = '2000000';
-        $pawnshop->order_in = 1;
-        $pawnshop->order_out = 1;
-        $pawnshop->bank_order_in = 1;
-        $pawnshop->bank_order_out = 1;
-        $pawnshop->save();
-
-        $pawnshop = new Pawnshop();
         $pawnshop->city = 'Գյումրի';
         $pawnshop->address = 'Գորկու 68/3';
         $pawnshop->license = 'ԳԳ-Ե 246';
@@ -58,6 +37,25 @@ class PawnshopSeeder extends Seeder
         $pawnshop->bank_order_out = 1;
         $pawnshop->save();
 
-
+        $pawnshop = new Pawnshop();
+        $pawnshop->city = 'Երևան';
+        $pawnshop->address = 'Արշակունյաց պ. 53/35';
+        $pawnshop->license = 'ԳԳ-Ե 302';
+        $pawnshop->representative = 'Վ. Սահակյան';
+        $pawnshop->telephone = '(010) 49-07-77';
+        $pawnshop->phone1 = '096 99 91 80';
+        $pawnshop->phone2 = '094 97 19 51';
+        $pawnshop->email = 'diamondcredit1@mail.ru';
+        $pawnshop->bank = 'Ամերիաբանկ';
+        $pawnshop->card_account_number = '1570073548790300';
+        $pawnshop->worth = '0';
+        $pawnshop->given = '0';
+        $pawnshop->cashbox = '5000000';
+        $pawnshop->bank_cashbox = '2000000';
+        $pawnshop->order_in = 1;
+        $pawnshop->order_out = 1;
+        $pawnshop->bank_order_in = 1;
+        $pawnshop->bank_order_out = 1;
+        $pawnshop->save();
     }
 }
