30843016c4ce920ea2bea00791d6fff8ecce722f
Ani Mashinyan <ani.mashinyan00@gmail.com>
2025-06-10 23:32:28 +0400

    test

--- app/Traits/ContractTrait.php
+++ app/Traits/ContractTrait.php
@@ -388,7 +388,7 @@ trait ContractTrait
             $contract->save();
 
             return [
-                'penalty_amount' => $total_penalty_amount,
+                'penalty_amount' => $penalty_amount,
 //                'penalty_amount' => max($total_penalty_amount, 0),
                 'delay_days' => $total_delay_days,
             ];
