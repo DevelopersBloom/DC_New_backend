9507c50ff27fbcbeebe438d6c8d37406d3627740
Ani Mashinyan <ani.mashinyan00@gmail.com>
2025-07-14 22:18:53 +0400

    Fix payment mother amount

--- app/Services/ContractService.php
+++ app/Services/ContractService.php
@@ -398,19 +398,20 @@ class   ContractService
             $paymentDate  = $nextPaymentDate->lt($toDate) ? $nextPaymentDate : $toDate;
 
             $diffDays = $paymentDate->diffInDays($currentDate);
-       
+            $payment['mother'] = 0;
+
             // Check if it's the last payment
             if ($paymentDate->eq($toDate)) {
                 $diffDays++;
                 $payment['mother'] = $contract->provided_amount; // Add mother amount for the last payment
                 $payment['last_payment'] = true;
             }
-                 $amount = $this->calcAmount($contract->provided_amount, $diffDays, $contract->interest_rate);
+
+            $amount = $this->calcAmount($contract->provided_amount, $diffDays, $contract->interest_rate);
             $payment['date'] = $paymentDate->format('Y-m-d');
             $payment['days'] = $diffDays;
             $payment['amount'] = $amount;
             $payment['pawnshop_id'] = $pawnshop_id;
-            $payment['mother'] = 0;
             $payment['PGI_ID'] = $pgi_id;
 
             Payment::create($payment);
