d426e5b6dfed8e1d64a2abc099373733d230a859
Ani Mashinyan <ani.mashinyan00@gmail.com>
2025-08-05 21:33:54 +0400

    test

--- app/Exports/PaymentsExport.php
+++ app/Exports/PaymentsExport.php
@@ -37,8 +37,8 @@ class PaymentsExport implements FromCollection, WithHeadings, WithStyles
                 $payment->PGI_ID,
                 $payment->contract_id,
                 $payment->date,
-                $amount ?? 0,
-                $payment->paid ?? 0,
+                (float) $amount ?? 0,
+                (float) $payment->paid ?? 0,
                 $payment->mother,
                 $status,
             ];
