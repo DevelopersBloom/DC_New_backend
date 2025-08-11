df2e36d467f0b2e1cfe509e2963e825315dceb71
Ani Mashinyan <ani.mashinyan00@gmail.com>
2025-08-11 21:37:29 +0400

    conteact import

--- app/Console/Commands/RunOnceCommand.php
+++ app/Console/Commands/RunOnceCommand.php
@@ -36,8 +36,8 @@ class RunOnceCommand extends Command
     public function handle()
     {
         $this->info('Started Executing');
-        $contractFilePath = base_path('ImportContractNewData.xlsx');
-        Excel::import(app(ContractsImportNewData::class), $contractFilePath);
+        $contractFilePath = base_path('ImportContract.xlsx');
+        Excel::import(app(ContractImportNew::class), $contractFilePath);
         $this->info('Contracts Executed');
 
 //        $this->info('Started items import');
@@ -46,9 +46,9 @@ class RunOnceCommand extends Command
 //        $this->info('Items Executed');
 //
         $this->info('Payment Executing');
-        $paymentFilePath = base_path('ImportPaymentNewData.xlsx');
+        $paymentFilePath = base_path('ImportPayment.xlsx');
         $paymentService = app(PaymentService::class);
-        Excel::import(new PaymentImportNewData($paymentService), $paymentFilePath);
+        Excel::import(new PaymentImportNew($paymentService), $paymentFilePath);
         $this->info('Payments Executed');
 
 //        $paymentFilePath1 = base_path('PaymentImport.xlsx');
