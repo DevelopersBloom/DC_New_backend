77313f83f65645d87de3f0e8eb5b0dd9ad915c05
Ani Mashinyan <ani.mashinyan00@gmail.com>
2025-09-14 19:07:22 +0400

    add access type in loan ndm table

--- app/Http/Requests/StoreLoanNdmRequest.php
+++ app/Http/Requests/StoreLoanNdmRequest.php
@@ -29,23 +29,26 @@ class StoreLoanNdmRequest extends FormRequest
             'client_id' => ['required', 'exists:clients,id'],
             'name' => ['required', 'string', 'max:255'],
             'currency_id' => ['required', 'exists:currencies,id'],
+
             'account_id' => ['nullable', 'exists:chart_of_accounts,id'],
             'interest_account_id' => ['nullable', 'exists:chart_of_accounts,id'],
             'amount' => ['required', 'numeric', 'min:0.01'],
-            'calculate_first_day' => ['boolean'],
+
             'contract_date' => ['required', 'date'],
             'disbursement_date' => ['required', 'date'],
             'maturity_date' => ['nullable', 'date'],
             'comment' => ['nullable', 'string'],
+
             'pawnshop_id' => ['required', 'exists:pawnshops,id'],
+            'access_type' => ['nullable',Rule::in(['loan', 'exchange', 'overdraft'])],
 
             'interest_schedule_mode' => ['nullable', Rule::in(['fixed_day_of_month', 'periodicity', 'last_date'])],
             'repayment_start_date' => ['nullable', 'date'],
             'repayment_end_date' => ['nullable', 'date', 'after:repayment_start_date'],
 
             'day_count_convention' => ['required', Rule::in(['calendar_year', 'days_360', 'fixed_day'])],
-            'access_type' => ['nullable',Rule::in(['loan', 'exchange', 'overdraft'])],
             'interest_rate' => ['nullable', 'numeric', 'between:0,100'],
+
             'tax_rate' => ['nullable', 'numeric', 'between:0,100'],
             'effective_interest_rate' => ['nullable', 'numeric', 'between:0,100'],
             'actual_interest_rate' => ['nullable', 'numeric', 'between:0,100'],
