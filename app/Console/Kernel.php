a12c5129874328886d9557ee7f47c9b70967246c
Ani Mashinyan <ani.mashinyan00@gmail.com>
2025-10-14 22:31:00 +0400

    Add client classification job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->job(new UpdateClientClassifications)
-            ->dailyAt('22:25')
+            ->dailyAt('22:35')
             ->timezone('Asia/Yerevan')
             ->withoutOverlapping()
             ->onOneServer();
