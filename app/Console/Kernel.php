106c658230f62b641f42804f709064051665c5a9
Ani Mashinyan <ani.mashinyan00@gmail.com>
2025-10-15 21:32:01 +0400

    Add contract client new functionalities

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->job(new UpdateClientClassifications)
-            ->dailyAt('21:30')
+            ->dailyAt('21:35')
             ->timezone('Asia/Yerevan')
             ->withoutOverlapping(10)
             ->appendOutputTo(storage_path('logs/schedule.log'));
