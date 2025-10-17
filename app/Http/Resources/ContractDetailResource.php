947d3d03a8f05b721014947015e5fcc345cb8dde
Ani Mashinyan <ani.mashinyan00@gmail.com>
2025-10-17 22:59:48 +0400

    Add classification in contract detial resource

--- app/Http/Resources/ContractDetailResource.php
+++ app/Http/Resources/ContractDetailResource.php
@@ -62,7 +62,7 @@ class ContractDetailResource extends JsonResource
                 'passport_issued'   => $this->client->passport_issued,
                 'is_linked_to_company' => (bool)$this->client->is_linked_to_company,
                 'is_company_employee' => (bool)$this->client->is_company_employee,
-                'classification' => $this->classification?->name,
+                'classification' => $this->client->classification?->name,
             ],
             'payments' => $this->payments->map(function ($payment) {
                 return [
