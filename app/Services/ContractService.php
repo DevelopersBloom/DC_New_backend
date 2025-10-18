7b9e2b0516b207b8d5e3ed7b2e9e94f4510b9f20
Ani Mashinyan <ani.mashinyan00@gmail.com>
2025-10-18 15:29:47 +0400

    Add payment type in store contract

--- app/Services/ContractService.php
+++ app/Services/ContractService.php
@@ -281,7 +281,7 @@ class   ContractService
             'pawnshop_id' => auth()->user()->pawnshop_id ?? $data['pawnshop_id'],
             'user_id' => auth()->user()->id ?? 1,
             'category_id' => $data['category_id'] ?? null,
-            'payment_type' => 'amortized',
+            'payment_type' => $data['payment_type'],
         ];
 
         // Create and return the contract
