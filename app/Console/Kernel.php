1887bf9e764a1a9c93116754d99c7f726ab16a25
Ani Mashinyan <ani.mashinyan00@gmail.com>
2025-10-22 17:02:39 +0400

    Fix effective amount

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
             ->appendOutputTo(storage_path('logs/schedule.log'));
 
         $schedule->job(new ProcessContractDailyRate)
-            ->dailyAt('16:00')
+            ->dailyAt('16:04')
             ->timezone('Asia/Yerevan')
             ->withoutOverlapping(10)
             ->appendOutputTo(storage_path('logs/schedule.log'));
