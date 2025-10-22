f033eb55c147c0cb3ff35bcc43d28f9630930607
Ani Mashinyan <ani.mashinyan00@gmail.com>
2025-10-22 17:02:33 +0400

    add effective amount in payments

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
             ->appendOutputTo(storage_path('logs/schedule.log'));
 
         $schedule->job(new ProcessContractDailyRate)
-            ->dailyAt('15:50')
+            ->dailyAt('16:00')
             ->timezone('Asia/Yerevan')
             ->withoutOverlapping(10)
             ->appendOutputTo(storage_path('logs/schedule.log'));
