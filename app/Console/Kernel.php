40d704648b49773c41c9ce14ccc51da082952727
Ani Mashinyan <ani.mashinyan00@gmail.com>
2025-10-23 22:43:09 +0400

    Fix schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,7 +18,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->job(new UpdateClientClassifications)
-            ->dailyAt('22:43')
+            ->dailyAt('22:44')
             ->timezone('Asia/Yerevan')
             ->withoutOverlapping(10)
             ->appendOutputTo(storage_path('logs/schedule.log'));
