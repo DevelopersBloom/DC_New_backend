3c8b645e733e0362a08e48fa3fe26acf9e34f132
Ani Mashinyan <ani.mashinyan00@gmail.com>
2025-10-23 16:50:19 +0400

    Fix schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
             ->appendOutputTo(storage_path('logs/schedule.log'));
 
         $schedule->job(new ProcessContractDailyRate)
-            ->dailyAt('15:18')
+            ->dailyAt('16:52')
             ->timezone('Asia/Yerevan')
             ->withoutOverlapping(10)
             ->appendOutputTo(storage_path('logs/schedule.log'));
