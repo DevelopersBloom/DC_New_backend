2c28eae4844cd453d157c00ffb939546daf42e29
Ani Mashinyan <ani.mashinyan00@gmail.com>
2025-10-23 11:06:26 +0400

    change schedue  hour

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
             ->appendOutputTo(storage_path('logs/schedule.log'));
 
         $schedule->job(new ProcessContractDailyRate)
-            ->dailyAt('17:16')
+            ->dailyAt('11:15')
             ->timezone('Asia/Yerevan')
             ->withoutOverlapping(10)
             ->appendOutputTo(storage_path('logs/schedule.log'));
