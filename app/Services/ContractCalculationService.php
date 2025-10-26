3f0158ce28d03fd1727ded0def2c1bc7b49b38ae
Ani Mashinyan <ani.mashinyan00@gmail.com>
2025-10-26 10:49:27 +0400

    CLient classification schedule fix

--- app/Services/ContractCalculationService.php
+++ app/Services/ContractCalculationService.php
@@ -125,7 +125,7 @@ class ContractCalculationService
     {
         $writtenOff = null;
 
-        if (($contract->client->classification ?? null) === 'loss') {
+        if (($contract->client->classification->name) === 'loss') {
             $writtenOff = max(0, (float)($contract->provided_amount ?? 0));
         }
         $contract->written_off_amount = $writtenOff !== null ? round($writtenOff, 2) : null;
