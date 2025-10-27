73488846a67ecb8c437e1eebfa6c913df8b16e5d
Ani Mashinyan <ani.mashinyan00@gmail.com>
2025-10-27 19:37:58 +0400

    Add interest calc in contract

--- app/Http/Controllers/ContractControllerNew.php
+++ app/Http/Controllers/ContractControllerNew.php
@@ -392,6 +392,7 @@ class ContractControllerNew extends Controller
 
     public function calculateContractInterest(Request $request)
     {
+        dd(2);
         $request->validate([
             'contract_id' => 'required|integer|exists:contracts,id',
             'calc_date' => 'required|date',
