c01cc9edf0293c3f5eeb65d2977708c13b740799
Ani Mashinyan <ani.mashinyan00@gmail.com>
2025-10-29 22:12:31 +0400

    Add transaction export

--- routes/api.php
+++ routes/api.php
@@ -141,7 +141,7 @@ Route::group(['middleware' => 'jwt.auth'], function () {
         Route::get('/loan-ndm/by-journal/{journal}', [LoanNdmController::class, 'get']);
         Route::get('/transactions', [TransactionController::class, 'index']);
         Route::get('/transactions/export', [TransactionController::class, 'export']);
-        Route::get('/transactions/account-balance/export', [TransactionController::class, 'export']);
+        Route::get('/transactions/account-balance/export', [TransactionController::class, 'exportAccountBalance']);
 
         Route::get('/transactions/loan-ndms', [DocumentJournalController::class, 'index']);
         Route::get('/documents-journal/trashed', [DocumentJournalController::class, 'trashed']);
