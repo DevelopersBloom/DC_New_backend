395b8d34a96c1ff04d0e0adfac4edd62ba359a7d
Ani Mashinyan <ani.mashinyan00@gmail.com>
2025-10-30 23:23:48 +0400

    Add chart of account seeder

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,7 +18,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->job(new UpdateClientClassifications)
-            ->dailyAt('23:20')
+            ->dailyAt('23:24')
             ->timezone('Asia/Yerevan')
             ->withoutOverlapping(10)
             ->appendOutputTo(storage_path('logs/schedule.log'));
