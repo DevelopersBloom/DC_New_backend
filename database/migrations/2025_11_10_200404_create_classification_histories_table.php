c7043d5513c0116927153cf02ebf44fd7095c57c
Ani Mashinyan <ani.mashinyan00@gmail.com>
2025-11-10 20:42:07 +0400

    Add client classification change table

--- database/migrations/2025_11_10_200404_create_classification_histories_table.php
+++ database/migrations/2025_11_10_200404_create_classification_histories_table.php
@@ -8,7 +8,7 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('client_classification_histories', function (Blueprint $table) {
+        Schema::create('classification_histories', function (Blueprint $table) {
             $table->id();
             $table->foreignId('client_id')->constrained()->cascadeOnDelete();
             $table->foreignId('classification_id')->nullable()->constrained('clients_classification')->nullOnDelete();
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->morphs('actionable');
             $table->json('meta')->nullable();
             $table->timestamp('date')->default(now());
-            $table->index(['actionable_type', 'actionable_id'], 'client_class_hist_action_idx');
+            $table->index(['actionable_type', 'actionable_id'], 'class_hist_action_idx');
 
             $table->timestamps();
         });
