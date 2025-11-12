c30fe6807424905a7f005d5b3a16138ee60d1357
Ani Mashinyan <ani.mashinyan00@gmail.com>
2025-11-13 00:23:59 +0400

    Fix reserve amount in contract amount payment

--- app/Http/Controllers/ClientControllerNew.php
+++ app/Http/Controllers/ClientControllerNew.php
@@ -333,10 +333,10 @@ class ClientControllerNew extends Controller
                     ->where('journalable_id', $contract->id)
                     ->first();
 
-                $amount = 0;
+                $reserveAmount = 0;
 
                 if ($journal) {
-                    $amount = DocumentJournal::where('journalable_type', DocumentJournal::class)
+                    $reserveAmount = DocumentJournal::where('journalable_type', DocumentJournal::class)
                         ->where('journalable_id', $journal->id)
                         ->where(function ($q) {
                             $q->where('document_type', DocumentJournal::RESERVE_SPECIAL_AMOUNT)
@@ -344,6 +344,9 @@ class ClientControllerNew extends Controller
                         })
                         ->sum('amount_amd');
                 }
+                $amount = ($contract->provided_amount + $reserveAmount) * $newReservePercent / 100;
+                $oldReserveAmount = ($contract->provided_amount+$reserveAmount) * $oldReservePercent / 100;
+
                 if ($amount <= 0 && $client->classification->name !== 'loss') {
                     continue;
                 }
@@ -483,7 +486,7 @@ class ClientControllerNew extends Controller
                         'date' => now()->toDateString(),
                         'document_number' => $nextDocNum,
                         'document_type' => $classificationType,
-                        'amount_amd' => $amount,
+                        'amount_amd' => $oldReserveAmount,
                         'partner_id' => $clientId,
                         'credit_partner_id' => $clientId,
                         'comment' => "Old reserve for contract #{$contract->id} due to classification change",
@@ -504,7 +507,7 @@ class ClientControllerNew extends Controller
                         'credit_account_id' => $creditClassification,
                         'credit_currency_id' => 1,
                         'credit_partner_id' => $clientId,
-                        'amount_amd' => $amount,
+                        'amount_amd' => $oldReserveAmount,
                         'comment' => "Old reserve for contract #{$contract->id}",
                         'user_id' => auth()->id() ?? 1,
                         'is_system' => true,
