f3d6e851c5ae68de6d8ede75451583e9c8996d18
Ani Mashinyan <ani.mashinyan00@gmail.com>
2025-11-15 20:06:19 +0400

    Fix sheet 3 sheet4

--- app/Exports/V17Export.php
+++ app/Exports/V17Export.php
@@ -236,6 +236,46 @@ class V17Export
             $sheet2->setCellValue($col . '19', $data['amount']);
             $sheet2->setCellValue(chr(ord($col) + 1) . '19', $data['amount'] > 0 ? round(($data['weighted'] / $data['amount']) * 100, 2) : 0);
         }
+        // ---------- sheet 3 ----------
+        $sheet3 = $spreadsheet->getSheetByName('Sheet3');
+        $groups3 = [
+            'C' => ['amount' => 0, 'weighted' => 0],
+            'E' => ['amount' => 0, 'weighted' => 0],
+            'G' => ['amount' => 0, 'weighted' => 0],
+            'I' => ['amount' => 0, 'weighted' => 0],
+            'K' => ['amount' => 0, 'weighted' => 0],
+            'M' => ['amount' => 0, 'weighted' => 0],
+            'O' => ['amount' => 0, 'weighted' => 0],
+            'Q' => ['amount' => 0, 'weighted' => 0],
+        ];
+
+        foreach ($docs as $doc) {
+            $contract = $doc->journalable_type === 'App\Models\Contract' ? $doc->journalable : null;
+            if (!$contract) continue;
+
+            $days = Carbon::parse($contract->deadline)
+                ->diffInDays(Carbon::parse($contract->date));
+
+            $col = $this->getColumnByDays($days);
+            $amount = $contract->provided_amount;
+            $rate = $contract->effective_annual_rate ?? 0;
+            $groups3[$col]['amount'] += $amount;
+            $groups3[$col]['weighted'] += $amount * ($rate / 100);
+        }
+
+        foreach ($groups3 as $col => $data) {
+            $sheet3->setCellValue($col . '8', $data['amount']);
+            $sheet3->setCellValue(chr(ord($col) + 1) . '8', $data['amount'] > 0 ? round(($data['weighted'] / $data['amount']) * 100, 2) : 0);
+        }
+
+        // ---------- sheet 4 ----------
+        $sheet4 = $spreadsheet->getSheetByName('Sheet4');
+        $groups4 = $groups3;
+
+        foreach ($groups4 as $col => $data) {
+            $sheet4->setCellValue($col . '14', $data['amount']);
+            $sheet4->setCellValue(chr(ord($col) + 1) . '14', $data['amount'] > 0 ? round(($data['weighted'] / $data['amount']) * 100, 2) : 0);
+        }
 
         $fileName = 'v17_export_' . now()->format('Ymd_His') . '.xls';
         $path = storage_path('app/public/' . $fileName);
