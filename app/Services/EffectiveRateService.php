9c08a6a2e3977c18a0cbfc0fce41b5019e278505
Ani Mashinyan <ani.mashinyan00@gmail.com>
2025-11-19 21:55:30 +0400

    Fix irr

--- app/Services/EffectiveRateService.php
+++ app/Services/EffectiveRateService.php
@@ -146,35 +146,77 @@ class EffectiveRateService
         ];
     }
 
-    private function irr(array $cashflows, $guess = 0.1): ?float
+//    private function irr(array $cashflows, $guess = 0.1): ?float
+//    {
+//        $maxIterations = 100;
+//        $precision = 1e-7;
+//
+//        $rate = $guess;
+//        for ($i = 0; $i < $maxIterations; $i++) {
+//            $npv = 0.0;
+//            $derivative = 0.0;
+//            foreach ($cashflows as $t => $cf) {
+//                if (1 + $rate == 0) {
+//                    return null;
+//                }
+//                $npv += $cf / pow(1 + $rate, $t);
+//                $derivative += -$t * $cf / pow(1 + $rate, $t + 1);
+//            }
+//            if ($derivative == 0) {
+//                return null;
+//            }
+//
+//            $newRate = $rate - $npv / $derivative;
+//            if (!is_finite($newRate)) {
+//                return null;
+//            }
+//            if (abs($newRate - $rate) < $precision) {
+//                return $newRate;
+//            }
+//            $rate = $newRate;
+//        }
+//        return null;
+//    }
+    private function irrHighPrecision(array $cashflows, int $scale = 12): ?string
     {
-        $maxIterations = 100;
-        $precision = 1e-7;
+        $guess = "0.1";
+        $maxIterations = 50;
 
-        $rate = $guess;
         for ($i = 0; $i < $maxIterations; $i++) {
-            $npv = 0.0;
-            $derivative = 0.0;
+            $npv = "0";
+            $derivative = "0";
+
             foreach ($cashflows as $t => $cf) {
-                if (1 + $rate == 0) {
-                    return null;
-                }
-                $npv += $cf / pow(1 + $rate, $t);
-                $derivative += -$t * $cf / pow(1 + $rate, $t + 1);
+                $tStr = (string)$t;
+
+                // (1 + rate)^t
+                $pow = bcpow(bcadd("1", $guess, $scale), $tStr, $scale);
+
+                // NPV += cf / pow
+                $npv = bcadd($npv, bcdiv((string)$cf, $pow, $scale), $scale);
+
+                // derivative += -t * cf / (1 + rate)^(t+1)
+                $pow2 = bcpow(bcadd("1", $guess, $scale), bcadd($tStr, "1"), $scale);
+                $term = bcmul("-{$t}", bcdiv((string)$cf, $pow2, $scale), $scale);
+                $derivative = bcadd($derivative, $term, $scale);
             }
-            if ($derivative == 0) {
+
+            if ($derivative == "0") {
                 return null;
             }
 
-            $newRate = $rate - $npv / $derivative;
-            if (!is_finite($newRate)) {
-                return null;
+            // newRate = rate - npv / derivative
+            $newRate = bcsub($guess, bcdiv($npv, $derivative, $scale), $scale);
+
+            // Stop condition
+            if (bccomp(bcabs(bcsub($newRate, $guess, $scale)), "0.000000000001", $scale) === 0) {
+                return $newRate; // STRING, full precision, NO ROUNDING
             }
-            if (abs($newRate - $rate) < $precision) {
-                return $newRate;
-            }
-            $rate = $newRate;
+
+            $guess = $newRate;
         }
+
         return null;
     }
+
 }
