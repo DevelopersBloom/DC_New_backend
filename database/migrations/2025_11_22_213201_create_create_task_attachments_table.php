54c7d023dcede10e40d669001bad54cc2e3ad37a
Ani Mashinyan <ani.mashinyan00@gmail.com>
2025-11-22 22:29:47 +0400

    Add task crud

--- database/migrations/2025_11_22_213201_create_create_task_attachments_table.php
+++ database/migrations/2025_11_22_213201_create_create_task_attachments_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('create_task_attachments', function (Blueprint $table) {
+        Schema::create('task_attachments', function (Blueprint $table) {
             $table->id();
 
             $table->unsignedBigInteger('task_id');
