789d65f36d37b548d0955c9f886d6a1e6f9647c9
Ani Mashinyan <ani.mashinyan00@gmail.com>
2025-11-24 21:06:01 +0400

    Change v0 report type from xls to xlsx

--- app/Exports/V03Export.php
+++ app/Exports/V03Export.php
@@ -23,7 +23,7 @@ class V03Export
         $sheet1 = $spreadsheet->getSheetByName('Sheet1');
         $sheet1->setCellValue('D10', 'Ակրեդիտ');
         $sheet1->setCellValue('D11', Carbon::parse($from)->format('d.m.Y'));
-        $sheet1->setCellValue('F11', Carbon::parse($to)->format('d.m.Y'));
+        $sheet1->setCellValue('F11', 26);
 
         // ---------------------------
         // SHEET 2
