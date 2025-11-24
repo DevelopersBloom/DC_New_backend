4897f828092e6d328ef5cab2db1d789a0e20003d
Ani Mashinyan <ani.mashinyan00@gmail.com>
2025-11-24 20:37:39 +0400

    Change v0 report type from xls to xlsx

--- app/Exports/V03Export.php
+++ app/Exports/V03Export.php
@@ -98,16 +98,19 @@ class V03Export
                 }
             }
         }
-
-//        $fileName = 'v03_export_' . now()->format('Ymd_His') . '.xls';
         $fileName = 'v03_export_' . now()->format('Ymd_His') . '.xlsx';
         $filePath = storage_path('app/public/' . $fileName);
         $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
         $writer->save($filePath);
 
-        $writer = new Xls($spreadsheet);
-        $writer->save($filePath);
-
         return $filePath;
+
+//        $fileName = 'v03_export_' . now()->format('Ymd_His') . '.xls';
+//        $filePath = storage_path('app/public/' . $fileName);
+//
+//        $writer = new Xls($spreadsheet);
+//        $writer->save($filePath);
+//
+//        return $filePath;
     }
 }
