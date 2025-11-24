135b3fad1a3b44c08a05358fb8e1d55b9b44c897
Ani Mashinyan <ani.mashinyan00@gmail.com>
2025-11-24 21:38:23 +0400

    Fix date format for sheet two)

--- app/Exports/V03Export.php
+++ app/Exports/V03Export.php
@@ -22,8 +22,10 @@ class V03Export
         // ---------------------------
         $sheet1 = $spreadsheet->getSheetByName('Sheet1');
         $sheet1->setCellValue('D10', 'Ակրեդիտ');
-        $sheet1->setCellValue('D11', Carbon::parse($from)->format('d.m.Y'));
-        $sheet1->setCellValue('F11', \PhpOffice\PhpSpreadsheet\Shared\Date::PHPToExcel(Carbon::parse($to)->toDateTime()));
+//        $sheet1->setCellValue('D11', Carbon::parse($from)->format('d.m.Y'));
+//        $sheet1->setCellValue('F11', \PhpOffice\PhpSpreadsheet\Shared\Date::PHPToExcel(Carbon::parse($to)->toDateTime()));
+        $sheet1->setCellValue('D11', 10);
+        $sheet1->setCellValue('F11', 29);
 
         // ---------------------------
         // SHEET 2
