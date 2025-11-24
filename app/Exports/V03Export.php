ee38fed5bc04e5f621b8ce0449a26053248883db
Ani Mashinyan <ani.mashinyan00@gmail.com>
2025-11-24 21:42:04 +0400

    Fix date format for sheet two)

--- app/Exports/V03Export.php
+++ app/Exports/V03Export.php
@@ -22,80 +22,62 @@ class V03Export
         // ---------------------------
         $sheet1 = $spreadsheet->getSheetByName('Sheet1');
         $sheet1->setCellValue('D10', 'Ակրեդիտ');
-//        $sheet1->setCellValue('D11', Carbon::parse($from)->format('d.m.Y'));
-//        $sheet1->setCellValue('F11', \PhpOffice\PhpSpreadsheet\Shared\Date::PHPToExcel(Carbon::parse($to)->toDateTime()));
-        $sheet1->setCellValue('D11', 10);
-        $sheet1->setCellValue('F11', 29);
+
+        $startDay = Carbon::parse($from)->day;
+        $endDay = Carbon::parse($to)->day;
+
+        $sheet1->setCellValue('D11', $startDay);
+        $sheet1->setCellValue('F11', $endDay);
 
         // ---------------------------
         // SHEET 2
         // ---------------------------
         $sheet2 = $spreadsheet->getSheetByName('Sheet2');
-//        $sheet2->setCellValue('C3', \PhpOffice\PhpSpreadsheet\Shared\Date::PHPToExcel(Carbon::parse($from)->toDateTime()));
-//        $sheet2->setCellValue('E3', \PhpOffice\PhpSpreadsheet\Shared\Date::PHPToExcel(Carbon::parse($to)->toDateTime()));
-        $sheet2->setCellValue('C3',10);
-        $sheet2->setCellValue('E3', 29);
-
-        $start = Carbon::parse($from)->startOfMonth();
-        $end = Carbon::parse($to)->endOfMonth();
+        $sheet2->setCellValue('C3', $startDay);
+        $sheet2->setCellValue('E3', $endDay);
 
         $acc50000 = ChartOfAccount::idByCode('50000');
         $acc52000 = ChartOfAccount::idByCode('52000');
         $acc52001 = ChartOfAccount::idByCode('52001');
 
         $row = 9;
+        $current = Carbon::parse($from);
 
-        for ($date = $start->copy(); $date->lte($end); $date->addDay()) {
+        while ($current->lte(Carbon::parse($to))) {
             $balance50000 = DocumentJournal::where('debit_account_id', $acc50000)
-                    ->whereDate('date', $date)
+                    ->whereDate('date', $current)
                     ->sum('amount_amd') * -1
                 + DocumentJournal::where('credit_account_id', $acc50000)
-                    ->whereDate('date', $date)
+                    ->whereDate('date', $current)
                     ->sum('amount_amd');
 
             $balance52000 = DocumentJournal::where('debit_account_id', $acc52000)
-                    ->whereDate('date', $date)
+                    ->whereDate('date', $current)
                     ->sum('amount_amd') * -1
                 + DocumentJournal::where('credit_account_id', $acc52000)
-                    ->whereDate('date', $date)
+                    ->whereDate('date', $current)
                     ->sum('amount_amd');
 
             $balance52001 = DocumentJournal::where('debit_account_id', $acc52001)
-                    ->whereDate('date', $date)
+                    ->whereDate('date', $current)
                     ->sum('amount_amd') * -1
                 + DocumentJournal::where('credit_account_id', $acc52001)
-                    ->whereDate('date', $date)
+                    ->whereDate('date', $current)
                     ->sum('amount_amd');
 
             $finalSum = $balance50000 + $balance52000 + $balance52001;
-
             $sheet2->setCellValue('B' . $row, $finalSum);
+
+            $current->addDay();
             $row++;
         }
+
+
+        // ---------------------------
+        // SHEET 6
+        // ---------------------------
         $sheet6 = $spreadsheet->getSheetByName('Sheet6');
-//        $sheet6->setCellValue('E10', 0);
-
-        $d10 = $sheet6->getCell('D10')->getValue();
         $sheet6->setCellValue('E10', '=IF(D10<=4,3,IF(D10=5,3.4,IF(D10=6,3.5,IF(D10=7,3.65,IF(D10=8,3.75,IF(D10=9,3.85,4))))))');
-
-        if ($d10 <= 4) {
-            $e10 = 3;
-        } elseif ($d10 == 5) {
-            $e10 = 3.4;
-        } elseif ($d10 == 6) {
-            $e10 = 3.5;
-        } elseif ($d10 == 7) {
-            $e10 = 3.65;
-        } elseif ($d10 == 8) {
-            $e10 = 3.75;
-        } elseif ($d10 == 9) {
-            $e10 = 3.85;
-        } else { // >=10
-            $e10 = 4;
-        }
-
-//        $sheet6->setCellValue('E10', $e10);
-
 
         // ---------------------------
         // SAVE XLS
