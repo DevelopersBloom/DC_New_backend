9e6b3f40b46a2cce9780d9cb015de24c89b9cd4e
Ani Mashinyan <ani.mashinyan00@gmail.com>
2025-11-25 20:57:01 +0400

    Add sheet3 functionality

--- app/Exports/V03Export.php
+++ app/Exports/V03Export.php
@@ -117,7 +117,7 @@ class V03Export
                 if ($risk === null) {
                     continue;
                 }
-
+dd($dailyAmounts[$risk]);
                 if (!isset($dailyAmounts[$risk])) {
                     continue;
                 }
