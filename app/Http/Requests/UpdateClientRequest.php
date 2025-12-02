461faa430240e85209e1931a4e5575261e9ce55f
Ani Mashinyan <ani.mashinyan00@gmail.com>
2025-12-02 19:46:39 +0400

    Fix passport series unique rule

--- app/Http/Requests/UpdateClientRequest.php
+++ app/Http/Requests/UpdateClientRequest.php
@@ -84,7 +84,7 @@ class UpdateClientRequest extends FormRequest
                         ->where('type', 'individual')
                         ->where('id', '!=', $clientId)
                         ->get();
-dd($exists);
+dd($exists,$clientId);
                     if ($exists) {
                         $fail('Այս անձնագրի սերիայով ֆիզիկական հաճախորդ արդեն գոյություն ունի!!։');
                     }
