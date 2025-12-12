b2fde8f2a5d05b9f40db826b8fb03acb51caee8f
Ani Mashinyan <ani.mashinyan00@gmail.com>
2025-12-12 22:04:25 +0400

    Add ndm daily rate calc

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -31,7 +31,7 @@ class Kernel extends ConsoleKernel
             ->appendOutputTo(storage_path('logs/schedule.log'));
 
         $schedule->job(new ProcessDailyNdmInterest)
-            ->dailyAt('22:03')
+            ->dailyAt('22:05')
             ->timezone('Asia/Yerevan')
             ->withoutOverlapping(10)
             ->appendOutputTo(storage_path('logs/schedule.log'));
