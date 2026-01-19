a24809fe82c42e647578b1f92a12ae9f4e9b20a0
AnMashinyan <ani.mashinyan00@gmail.com>
2026-01-19 22:17:08 +0400

    Add guarantors for contract

--- database/migrations/2026_01_19_211307_create_contract_guarantors_table.php
+++ database/migrations/2026_01_19_211307_create_contract_guarantors_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('contract_guarantors', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('client_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('contract_id')->constrained()->cascadeOnDelete();
             $table->foreignId('client_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
