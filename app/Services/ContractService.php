87f9a3d3f26ace7fc03807886b4beb3e132a0241
AnMashinyan <ani.mashinyan00@gmail.com>
2026-01-23 21:35:32 +0400

    Change contract number logic

--- app/Services/ContractService.php
+++ app/Services/ContractService.php
@@ -258,6 +258,8 @@ class   ContractService
     }
     public function createContract(int $client_id, array $data, $deadline)
     {
+
+        dd($data,$data['items'][0]);
         $categoryId = $data['items'][0]['category_id'];
 
         $contractNumber = $this->generateContractNumber($categoryId);
