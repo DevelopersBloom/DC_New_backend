1a1fdf593360f86f5eff59f01646f74b31c69154
AnMashinyan <ani.mashinyan00@gmail.com>
2026-02-08 17:19:46 +0400

    partial payment fix

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -259,7 +259,7 @@ class PaymentService
         }
 
         $totalRequiredForThisLine = $payment->amount + $penalty;
-
+dd($amount,$totalRequiredForThisLine);
         if ($amount >= $totalRequiredForThisLine) {
             $this->completePayment($payment, $payer, $cash, $contract->id, $deal_id);
         } else {
