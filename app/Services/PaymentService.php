f60e5c41245910745ba50dc8f30ce120f0b42ea9
AnMashinyan <ani.mashinyan00@gmail.com>
2026-02-08 12:41:41 +0400

    Change single payment logic for takeing account also principal payment

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -183,7 +183,6 @@ class PaymentService
     {
         $penalty = $payment->penalty ?? 0;
         $totalRequired = $payment->amount + $penalty;
-        dd($totalRequired);
 
         $paidDeal = DealAction::where('actionable_type', Payment::class)
             ->where('actionable_id', $payment->id)
@@ -191,6 +190,7 @@ class PaymentService
             ->first();
         $alreadyPaidTotal = data_get($paidDeal, 'history.payment_changes.0.new_paid', 0);
         $remainingInterest = max(0, $payment->interest_payment - $alreadyPaidTotal);
+        dd($totalRequired,$alreadyPaidTotal,$remainingInterest);
 
         $remainingAmount = $amount;
 
