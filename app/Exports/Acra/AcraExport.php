9b924e68f7e3583a2082038dc174b4ce0d48c0d3
AnMashinyan <ani.mashinyan00@gmail.com>
2026-02-12 21:10:24 +0400

    Fix sheet5 of acra report

--- app/Exports/Acra/AcraExport.php
+++ app/Exports/Acra/AcraExport.php
@@ -176,7 +176,7 @@ class AcraExport
                 ->where('date','<=', $this->to)
                 ->latest('date')
                 ->first();
-dd($lastPaymentDate,$contract->id);
+dd($journalId,$lastPaymentDate,$contract->id);
             if ($lastMotherPayment) {
                 $lastPaymentDate = $lastMotherPayment->date;
             } elseif ($contract->status === 'completed' || $contract->status === 'executed') {
