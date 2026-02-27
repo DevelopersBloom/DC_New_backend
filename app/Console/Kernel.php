259a6a87cff7ce3813dcda6d4cf23ef95784f31d
AnMashinyan <ani.mashinyan00@gmail.com>
2026-02-27 18:39:16 +0400

    Test eff daily rate calc

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
             ->appendOutputTo(storage_path('logs/schedule.log'));
 
         $schedule->job(new ProcessContractDailyRate)
-            ->dailyAt('18:39')
+            ->dailyAt('18:40')
             ->timezone('Asia/Yerevan')
             ->withoutOverlapping(10)
             ->appendOutputTo(storage_path('logs/schedule.log'));
