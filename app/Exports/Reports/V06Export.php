eaaddf74d8ac731a0dd4c2e722243064ab7aac2d
AnMashinyan <ani.mashinyan00@gmail.com>
2026-03-03 20:36:55 +0400

    Fix v06

--- app/Exports/Reports/V06Export.php
+++ app/Exports/Reports/V06Export.php
@@ -130,12 +130,12 @@ class V06Export
             if ($contract->category){
                 if ($contract->category->name === 'car') {
                     $carContractAmount += $amount ;
-                    $groupsCar['col'] += $net16200NV;
+                    $groupsCar[$col] += $net16200NV;
                     $carContractCount++;
                 }
                 if ($contract->category->name === 'gold') {
                     $goldContractAmount += $amount ;
-                    $groupsGold['col'] += $net16200NV;
+                    $groupsGold[$col] += $net16200NV;
                     $goldContractCount++;
                 }
                 if ($contract->category->name === 'electronics') {
