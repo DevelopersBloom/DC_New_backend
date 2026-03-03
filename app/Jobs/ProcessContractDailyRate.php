c8ad7e1902f6e9a4a84bbb4ed19bd8e23c28521c
AnMashinyan <ani.mashinyan00@gmail.com>
2026-03-03 21:43:20 +0400

    Fix daily rate calc

--- app/Jobs/ProcessContractDailyRate.php
+++ app/Jobs/ProcessContractDailyRate.php
@@ -244,11 +244,11 @@ class ProcessContractDailyRate implements ShouldQueue
 
     private function calculateCurrentAmortizedBalance(Contract $contract): float
     {
-        $initialProvided = (float)DocumentJournal::where('journalable_type', Contract::class)
-            ->where('journalable_id', $contract->id)
-            ->where('document_type', DocumentJournal::PROVIDE_CONTRACT_AMOUNT)
-            ->value('amount_amd') ?? (float)$contract->mother;
-
+        $initialProvided = (float)$contract->mother;
+//(float)DocumentJournal::where('journalable_type', Contract::class)
+//            ->where('journalable_id', $contract->id)
+//            ->where('document_type', DocumentJournal::PROVIDE_CONTRACT_AMOUNT)
+//            ->value('amount_amd')
         $fees = $initialProvided * ($contract->lump_rate / 100);
         $netAmount = $initialProvided - $fees;
 
