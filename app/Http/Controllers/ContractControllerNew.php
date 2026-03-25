77008971cbd0e708a9c52fa96bf1c691fcd36f26
AnMashinyan <ani.mashinyan00@gmail.com>
2026-03-26 00:06:36 +0400

    Fix calculated date in contract

--- app/Http/Controllers/ContractControllerNew.php
+++ app/Http/Controllers/ContractControllerNew.php
@@ -239,9 +239,10 @@ class ContractControllerNew extends Controller
             $pawnshop_id = \auth()->user()->pawnshop_id;
 //            $date = Carbon::now();
             $validatedContract = $contractRequest->validated();
-            $date = $validatedContract['contract_created_date']
-                ? Carbon::parse($validatedContract['contract_created_date'])
-                : Carbon::now('Asia/Yerevan');
+            $date = Carbon::parse($validatedContract['contract_created_date']);
+//            $validatedContract['contract_created_date']
+//                ? Carbon::parse($validatedContract['contract_created_date'])
+//                : Carbon::now('Asia/Yerevan');
             $deadline = $date->addMonths($contractRequest->validated()['deadline'])->format('Y-m-d');
             $contract = $this->contractService->createContract($client->id, $contractRequest->validated(), $deadline);
             $category_id = null;
