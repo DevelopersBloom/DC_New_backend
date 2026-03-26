2beee1e216fc9849e3f61dffa64fcae73b172100
AnMashinyan <ani.mashinyan00@gmail.com>
2026-03-26 19:08:59 +0400

    Fix partial payment

--- app/Models/DocumentJournal.php
+++ app/Models/DocumentJournal.php
@@ -406,7 +406,7 @@ class DocumentJournal extends Model
         $debitAcc = ChartOfAccount::where('code', $debitCode)->first();
         $creditAcc = ChartOfAccount::where('code', $creditCode)->first();
         $nextDocNum = (int) (Transaction::max('document_number') ?? 0) + 1;
-       dd($debitAcc, $creditAcc, $nextDocNum,$amount,$label);
+       dd($debitCode,$creditCode,$debitAcc, $creditAcc, $nextDocNum,$amount,$label);
         if (!$debitAcc || !$creditAcc) return;
 
         $childJournal = DocumentJournal::create([
