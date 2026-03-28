2b43c15c543c37dfce9295dd6df7f53b84f9095f
AnMashinyan <ani.mashinyan00@gmail.com>
2026-03-28 13:48:58 +0400

    Fix v20

--- app/Exports/Reports/V20Export.php
+++ app/Exports/Reports/V20Export.php
@@ -2,6 +2,7 @@
 
 namespace App\Exports\Reports;
 
+use App\Models\Client;
 use Illuminate\Support\Carbon;
 use PhpOffice\PhpSpreadsheet\Cell\DataType;
 use PhpOffice\PhpSpreadsheet\IOFactory;
@@ -20,6 +21,12 @@ class V20Export
         $sheet1 = $spreadsheet->getSheetByName('Sheet1');
         $sheet1->setCellValueExplicit('C5','«Ակրեդիտ» ՎՄ ՍՊԸ',DataType::TYPE_STRING);
         $sheet1->setCellValue('C6',Date::PHPToExcel($to));
+
+        $nonResidentCount = Client::where('residency_status', 'non_resident')->count();
+        $sheet1->setCellValue('C15', $nonResidentCount);
+
+        $residentCount = Client::where('residency_status', 'resident')->count();
+        $sheet1->setCellValue('C16', $residentCount);
 
         $fileName = 'v20_exprt_' . $from . '_' . $to . '.xls';
         $path = storage_path('app/public/' . $fileName);
