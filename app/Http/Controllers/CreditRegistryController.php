a8ad4004439fa9e9063023d7c8192b6579cb58b8
AnMashinyan <ani.mashinyan00@gmail.com>
2026-04-03 16:15:53 +0400

    Fix soap client

--- app/Http/Controllers/CreditRegistryController.php
+++ app/Http/Controllers/CreditRegistryController.php
@@ -26,6 +26,40 @@ class CreditRegistryController extends Controller
     ) {
     }
 
+    public function sendL001(string $id): Response
+    {
+        $contract = Contract::find($id);
+
+        if (! $contract) {
+            return response()->json(['message' => 'Contract not found'], 404);
+        }
+
+        $xml = $this->l001Service->generateL001Xml($contract);
+        $requestId = $this->soapClient->sendL001($xml, false);
+
+        $maxTries = 10;
+        $sleepMs = 500;
+
+        $isReady = false;
+        for ($i = 0; $i < $maxTries; $i++) {
+            if ($this->soapClient->isResponsePrepared($requestId)) {
+                $isReady = true;
+                break;
+            }
+            usleep($sleepMs * 1000);
+        }
+
+        $responseXml = null;
+        if ($isReady) {
+            $responseXml = $this->soapClient->getResponse($requestId);
+        }
+
+        return response()->json([
+            'request_id' => $requestId,
+            'is_ready' => $isReady,
+            'response_xml' => $responseXml,
+        ]);
+    }
     /**
      * Generate and download L001 XML for a single contract (Credit Registry).
      */
@@ -137,47 +171,5 @@ class CreditRegistryController extends Controller
 
         return response()->download($path, $filename)->deleteFileAfterSend(true);
     }
-    /**
-     * Send L001 XML for a single contract to CBA Credit Registry web service.
-     *
-     * Flow:
-     *  - Generate L001 XML from current Contract state
-     *  - Call SendRequest(AppName=LNREG3, DocType=L001, IsDelay=false, xml)
-     *  - Poll IsResponsePrepared
-     *  - If ready, call GetResponse and return response XML
-     */
-    public function sendL001(string $id): Response
-    {
-        $contract = Contract::find($id);
 
-        if (! $contract) {
-            return response()->json(['message' => 'Contract not found'], 404);
-        }
-
-        $xml = $this->l001Service->generateL001Xml($contract);
-        $requestId = $this->soapClient->sendL001($xml, false);
-
-        $maxTries = 10;
-        $sleepMs = 500;
-
-        $isReady = false;
-        for ($i = 0; $i < $maxTries; $i++) {
-            if ($this->soapClient->isResponsePrepared($requestId)) {
-                $isReady = true;
-                break;
-            }
-            usleep($sleepMs * 1000);
-        }
-
-        $responseXml = null;
-        if ($isReady) {
-            $responseXml = $this->soapClient->getResponse($requestId);
-        }
-
-        return response()->json([
-            'request_id' => $requestId,
-            'is_ready' => $isReady,
-            'response_xml' => $responseXml,
-        ]);
-    }
 }
