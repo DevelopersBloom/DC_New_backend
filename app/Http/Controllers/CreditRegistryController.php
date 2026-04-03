5a473a4288234079c519ca20f6be2f7d55cd08e6
AnMashinyan <ani.mashinyan00@gmail.com>
2026-04-03 22:18:19 +0400

    Fix soap client

--- app/Http/Controllers/CreditRegistryController.php
+++ app/Http/Controllers/CreditRegistryController.php
@@ -71,7 +71,8 @@ class CreditRegistryController extends Controller
         }
 
         $xml = $this->l001Service->generateL001Xml($contract);
-        $url = "http://100.100.100.60:8889/DEGSHost?singleWsdl";
+        $url = "https://100.100.100.60:8888/DEGSHost";
+//        $url = "http://100.100.100.60:8889/DEGSHost?singleWsdl";
 
         curl_setopt_array($ch, [
             CURLOPT_URL => $url,
