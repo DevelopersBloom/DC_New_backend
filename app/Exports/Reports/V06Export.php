e2315e363b7e06b36c62bff311e4a66d9dfe6f00
AnMashinyan <ani.mashinyan00@gmail.com>
2026-04-06 20:12:07 +0400

    V06

--- app/Exports/Reports/V06Export.php
+++ app/Exports/Reports/V06Export.php
@@ -30,20 +30,19 @@ class V06Export
 //            ->where('document_type', DocumentJournal::PROVIDE_CONTRACT_AMOUNT)
 //            ->whereDate('date', '<=', $date)
 //            ->get();
-        $docs = DocumentJournal::with([
-            'journalable.payments' => function ($q) use ($date) {
-                $q->where(function ($query) use ($date) {
-                    $query->where('status', 'initial')
-                        ->orWhere(function ($sub) use ($date) {
-                            $sub->where('status', 'completed')
-                                ->where('to_date', '<=', $date);
-                        });
-                });
-            }
-        ])
-            ->where('document_type', DocumentJournal::PROVIDE_CONTRACT_AMOUNT)
-            ->whereDate('date', '<=', $date)
-            ->get();
+
+        $docs = DocumentJournal::with(['journalable.payments' => function ($q) use ($date) {
+            $q->where(function ($query) use ($date) {
+                $query->where(function ($q) use ($date) {
+                    $q->where('status', 'initial')
+                        ->where('to_date', '<', $date);
+                })
+                    ->orWhere(function ($q) {
+                        $q->where('status', 'completed')
+                            ->whereColumn('date', '>', 'to_date');
+                    });
+            });
+        }]);
         $carContractCount = 0;
         $goldContractCount = 0;
         $electronicsContractCount = 0;
@@ -72,9 +71,22 @@ class V06Export
             $contract = $doc->journalable;
             if (!$contract || !$contract->client || !$contract->client->classification || $contract->status != 'initial') continue;
 
+//            $hasExpiredPayment = $contract->payments
+//                ->contains(function ($p) use ($date) {
+//                    return Carbon::parse($p->to_date)->lt($date);
+//                });
             $hasExpiredPayment = $contract->payments
                 ->contains(function ($p) use ($date) {
-                    return Carbon::parse($p->to_date)->lt($date);
+
+                    if ($p->status === 'initial' && Carbon::parse($p->to_date)->lt($date)) {
+                        return true;
+                    }
+
+                    if ($p->status === 'completed' && $p->paid_date && Carbon::parse($p->paid_date)->gt(Carbon::parse($p->to_date))) {
+                        return true;
+                    }
+
+                    return false;
                 });
 
             if ($hasExpiredPayment) {
