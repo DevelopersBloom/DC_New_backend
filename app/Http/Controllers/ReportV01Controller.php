c91cce927b86adc0f9aa132a3841f6a276903a9b
AnMashinyan <ani.mashinyan00@gmail.com>
2026-04-06 19:33:57 +0400

    Fix partner account balance filter

--- app/Http/Controllers/ReportV01Controller.php
+++ app/Http/Controllers/ReportV01Controller.php
@@ -66,37 +66,6 @@ class ReportV01Controller extends Controller
         if ($rows->isEmpty()) {
             $sheet->setCellValueExplicit("A{$currentRow}", '', DataType::TYPE_STRING);
         } else {
-//            foreach ($rows as $row) {
-//                $code = (string)$row->code;
-//
-//                if (preg_match('/^\d+$/', $code)) {
-//                    $sheet->setCellValueExplicitByColumnAndRow(1, $currentRow, (float)$code, DataType::TYPE_NUMERIC);
-//                } else {
-//                    $sheet->setCellValueExplicitByColumnAndRow(1, $currentRow, $code, DataType::TYPE_STRING);
-//                }
-//
-//
-//                $nums = [
-//                    6 => (float)($row->amd_resident ?? 0),
-//                    7 => (float)($row->amd_non_resident ?? 0),
-//                    8 => (float)($row->fx_group1_resident ?? 0),
-//                    9 => (float)($row->fx_group1_non_resident ?? 0),
-//                    10 => (float)($row->usd_resident ?? 0),
-//                    11 => (float)($row->usd_non_resident ?? 0),
-//                    12 => (float)($row->eur_resident ?? 0),
-//                    13 => (float)($row->eur_non_resident ?? 0),
-//                    14 => (float)($row->fx_group2_resident ?? 0),
-//                    15 => (float)($row->fx_group2_non_resident ?? 0),
-//                    16 => (float)($row->rub_resident ?? 0),
-//                    17 => (float)($row->rub_non_resident ?? 0),
-//                ];
-//
-//                foreach ($nums as $colIndex => $val) {
-//                    $sheet->setCellValueExplicitByColumnAndRow($colIndex, $currentRow, $val/1000, DataType::TYPE_NUMERIC);
-//                }
-//
-//                $currentRow++;
-//            }
             foreach ($rows as $row) {
                 $code = (string)$row->code;
 
@@ -106,7 +75,6 @@ class ReportV01Controller extends Controller
                     $sheet->setCellValueExplicitByColumnAndRow(1, $currentRow, $code, DataType::TYPE_STRING);
                 }
 
-                $sheet->setCellValueExplicitByColumnAndRow(2, $currentRow, (string)($row->name ?? ''), DataType::TYPE_STRING);
 
                 $nums = [
                     6 => (float)($row->amd_resident ?? 0),
@@ -124,15 +92,47 @@ class ReportV01Controller extends Controller
                 ];
 
                 foreach ($nums as $colIndex => $val) {
-                    if (abs($val) < 0.0001) {
-                        $sheet->setCellValueByColumnAndRow($colIndex, $currentRow, null);
-                    } else {
-                        $sheet->setCellValueExplicitByColumnAndRow($colIndex, $currentRow, $val/1000, DataType::TYPE_NUMERIC);
-                    }
+                    $sheet->setCellValueExplicitByColumnAndRow($colIndex, $currentRow, $val/1000, DataType::TYPE_NUMERIC);
                 }
 
                 $currentRow++;
             }
+//            foreach ($rows as $row) {
+//                $code = (string)$row->code;
+//
+//                if (preg_match('/^\d+$/', $code)) {
+//                    $sheet->setCellValueExplicitByColumnAndRow(1, $currentRow, (float)$code, DataType::TYPE_NUMERIC);
+//                } else {
+//                    $sheet->setCellValueExplicitByColumnAndRow(1, $currentRow, $code, DataType::TYPE_STRING);
+//                }
+//
+//                $sheet->setCellValueExplicitByColumnAndRow(2, $currentRow, (string)($row->name ?? ''), DataType::TYPE_STRING);
+//
+//                $nums = [
+//                    6 => (float)($row->amd_resident ?? 0),
+//                    7 => (float)($row->amd_non_resident ?? 0),
+//                    8 => (float)($row->fx_group1_resident ?? 0),
+//                    9 => (float)($row->fx_group1_non_resident ?? 0),
+//                    10 => (float)($row->usd_resident ?? 0),
+//                    11 => (float)($row->usd_non_resident ?? 0),
+//                    12 => (float)($row->eur_resident ?? 0),
+//                    13 => (float)($row->eur_non_resident ?? 0),
+//                    14 => (float)($row->fx_group2_resident ?? 0),
+//                    15 => (float)($row->fx_group2_non_resident ?? 0),
+//                    16 => (float)($row->rub_resident ?? 0),
+//                    17 => (float)($row->rub_non_resident ?? 0),
+//                ];
+//
+//                foreach ($nums as $colIndex => $val) {
+//                    if (abs($val) < 0.0001) {
+//                        $sheet->setCellValueByColumnAndRow($colIndex, $currentRow, null);
+//                    } else {
+//                        $sheet->setCellValueExplicitByColumnAndRow($colIndex, $currentRow, $val/1000, DataType::TYPE_NUMERIC);
+//                    }
+//                }
+//
+//                $currentRow++;
+//            }
         }
 
         $writer = new XlsxWriter($spreadsheet);
