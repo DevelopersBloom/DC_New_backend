c6b61611d36b9d2d721db22550b5c859cdd2114f
AnMashinyan <ani.mashinyan00@gmail.com>
2026-04-07 22:44:26 +0400

    Test penalty rate amount in daily rate job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
             ->appendOutputTo(storage_path('logs/schedule.log'));
 
         $schedule->job(new ProcessContractDailyRate)
-            ->dailyAt('22:09')
+            ->dailyAt('22:46')
             ->timezone('Asia/Yerevan')
             ->withoutOverlapping(10)
             ->appendOutputTo(storage_path('logs/schedule.log'));
