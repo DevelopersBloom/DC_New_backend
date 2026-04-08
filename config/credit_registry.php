a62e07dd8bd096dc3d18d9331551dfe6a36b7bfe
AnMashinyan <ani.mashinyan00@gmail.com>
2026-04-08 21:28:25 +0400

    FIx acra send

--- config/credit_registry.php
+++ config/credit_registry.php
@@ -46,5 +46,7 @@ return [
     // Default timeout settings (seconds)
     'connection_timeout' => env('CREDIT_REGISTRY_CONNECTION_TIMEOUT', 30),
     'response_timeout' => env('CREDIT_REGISTRY_RESPONSE_TIMEOUT', 180),
+    'username' => 'test',
+    'password' => 'test',
 ];
 
