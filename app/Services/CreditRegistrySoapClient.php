82f118c52b4b6089d6135cdb6812b2c59a9bece7
AnMashinyan <ani.mashinyan00@gmail.com>
2026-04-08 21:34:41 +0400

    FIx acra send

--- app/Services/CreditRegistrySoapClient.php
+++ app/Services/CreditRegistrySoapClient.php
@@ -171,7 +171,7 @@ class CreditRegistrySoapClient
 //        $ut->addChild('wsse:Username', $username, $wsse);
         $ut->addChild('wsse:Password', $password, $wsse)
             ->addAttribute('Type', 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText');
-dd($wsse, new \SoapVar($timestamp->asXML());
+dd($wsse, new \SoapVar($timestamp->asXML()));
         return new \SoapHeader($wsse, 'Security', new \SoapVar($timestamp->asXML(), XSD_ANYXML), true);
     }
     public function isResponsePrepared(int $requestId): bool
