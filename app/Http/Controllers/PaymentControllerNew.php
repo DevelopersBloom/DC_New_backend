0e055fcdde85d587b63a1b0bdfe3d6cf02833113
AnMashinyan <ani.mashinyan00@gmail.com>
2026-04-11 22:18:37 +0400

    FIx full payment

--- app/Http/Controllers/PaymentControllerNew.php
+++ app/Http/Controllers/PaymentControllerNew.php
@@ -97,6 +97,7 @@ class PaymentControllerNew extends Controller
                 throw new \RuntimeException('No payable rows found for this contract');
             }
             $order_id = null;
+            dd(34);
             if ($payments) {
                 $order_id = $this->generateOrderInNew($request, $payments, Order::REGULAR_FILTER)->id;
             }
