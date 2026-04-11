9f4000355fa313b7b0890e867b8f96ed1c6f83d4
AnMashinyan <ani.mashinyan00@gmail.com>
2026-04-11 22:02:20 +0400

    FIx full payment

--- app/Http/Controllers/PaymentControllerNew.php
+++ app/Http/Controllers/PaymentControllerNew.php
@@ -58,10 +58,10 @@ class PaymentControllerNew extends Controller
             $rawPaymentIds = $request->input('payment_ids', $request->input('payments', []));
 
             $currentPaymentAmount = $this->calculateCurrentPayment($contract);
-           dd($currentPaymentAmount);
             $currentInterest = $currentPaymentAmount['current_amount'];
             $penaltyAmount = $currentPaymentAmount['penalty_amount'];
-                $providedAmount = (float) $contract->provided_amount;
+            $providedAmount = (float) $contract->provided_amount;
+            dd($currentInterest,$penaltyAmount,$providedAmount);
 
             $fullPaymentThreshold = $currentInterest + $penaltyAmount + $providedAmount;
 
