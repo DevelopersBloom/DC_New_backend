53b3ff528ce9e7bcd9156d580af816ee67234a1b
AnMashinyan <ani.mashinyan00@gmail.com>
2026-04-13 21:04:15 +0400

    Fix payment for remaining interest

--- app/Http/Controllers/PaymentControllerNew.php
+++ app/Http/Controllers/PaymentControllerNew.php
@@ -107,6 +107,7 @@ class PaymentControllerNew extends Controller
                 ->where('journalable_id', $contract->id)
                 ->first();
             $forceScheduled = $paymentIds->isNotEmpty();
+            dd($payments->count());
             $result = $this->paymentService->processPayments(
                 $contract, $amount, $payer, $cash, $payments, $deal->id, $journal->id, $forceScheduled, $paymentDate,$interestAmount
             );
