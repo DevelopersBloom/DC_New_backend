619151a39b1888a41c1fb79e0c8def32799b15bd
AnMashinyan <ani.mashinyan00@gmail.com>
2026-04-13 23:52:23 +0400

    Fix payment for remaining interest

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -778,6 +778,7 @@ class PaymentService
         $rate = (float) $contract->interest_rate;
         $prevDate = Carbon::parse($contract->date);
         foreach ($payments as $payment) {
+
             $paymentDate = Carbon::parse($payment->to_date);
             $fromDate = Carbon::parse($payment->from_date);
             $selectedDate = $fromDate->lt(now()) ? now() : $fromDate;
