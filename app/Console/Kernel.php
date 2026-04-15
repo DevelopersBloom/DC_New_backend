7e1736536775d702fb6df4dc9a33729b8464d04e
AnMashinyan <ani.mashinyan00@gmail.com>
2026-04-15 23:22:42 +0400

    Add daily bank provision job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -38,7 +38,7 @@ class Kernel extends ConsoleKernel
             ->appendOutputTo(storage_path('logs/schedule.log'));
 
         $schedule->job(new ProcessDailyBankProvision)
-            ->dailyAt('23:21')
+            ->dailyAt('23:23')
             ->timezone('Asia/Yerevan')
             ->withoutOverlapping(10)
             ->appendOutputTo(storage_path('logs/schedule.log'));
