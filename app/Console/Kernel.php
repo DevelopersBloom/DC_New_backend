65e4b7c509422b0fcca9f66a96862d60cb556fb4
AnMashinyan <ani.mashinyan00@gmail.com>
2026-04-15 23:55:52 +0400

    Add daily bank provision job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -37,11 +37,11 @@ class Kernel extends ConsoleKernel
             ->withoutOverlapping(10)
             ->appendOutputTo(storage_path('logs/schedule.log'));
 
-        $schedule->job(new ProcessDailyBankProvision)
-            ->dailyAt('23:46')
-            ->timezone('Asia/Yerevan')
-            ->withoutOverlapping(10)
-            ->appendOutputTo(storage_path('logs/laravel.log'));
+//        $schedule->job(new ProcessDailyBankProvision)
+//            ->dailyAt('23:46')
+//            ->timezone('Asia/Yerevan')
+//            ->withoutOverlapping(10)
+//            ->appendOutputTo(storage_path('logs/laravel.log'));
     }
 
     /**
