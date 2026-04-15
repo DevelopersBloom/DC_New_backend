68068057b27bb5b60492a7c632ebb68ed4bc92e9
macbook <Grigor@MacBook-Pro-6.local>
2026-04-15 12:00:33 +0400

    full payment

--- app/Http/Controllers/DocumentJournalController.php
+++ app/Http/Controllers/DocumentJournalController.php
@@ -376,8 +376,9 @@ class DocumentJournalController
         }
 
         $allIds = [];
+        $visited = [];
         foreach ($rootDocs as $rootDoc) {
-            $this->collectAllRelatedIds($rootDoc, $allIds);
+            $this->collectAllRelatedIds($rootDoc, $allIds, $visited);
         }
         $allIds = array_values(array_unique($allIds));
 
@@ -395,8 +396,13 @@ class DocumentJournalController
         return response()->json(DocumentJournalResource::collection($documents));
     }
 
-    private function collectAllRelatedIds(DocumentJournal $doc, &$ids = []): array
+    private function collectAllRelatedIds(DocumentJournal $doc, array &$ids = [], array &$visited = []): array
     {
+        if (isset($visited[$doc->id])) {
+            return $ids;
+        }
+        $visited[$doc->id] = true;
+
         $ids[] = $doc->id;
 
         $children = DocumentJournal::where('journalable_id', $doc->id)
@@ -404,7 +410,7 @@ class DocumentJournalController
             ->get();
 
         foreach ($children as $child) {
-            $this->collectAllRelatedIds($child, $ids);
+            $this->collectAllRelatedIds($child, $ids, $visited);
         }
 
         return $ids;
