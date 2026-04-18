f8754efbc12fa648ace3de44c38cff5b631586e2
AnMashinyan <ani.mashinyan00@gmail.com>
2026-04-18 20:34:07 +0400

    Test payment

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -108,6 +108,7 @@ class PaymentService
                     $interestAmount = $result['remaining_interest'];
                     $interest_amount += $result['interest_amount'];
                     $principal_amount += $result['principal_amount'];
+                    dd($result);
                 }
             }
             if ($amount > 0) {
