93a79650e1c2d11500f26b28701904be2d350eed
AnMashinyan <ani.mashinyan00@gmail.com>
2026-04-18 20:34:51 +0400

    Test payment

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -108,9 +108,9 @@ class PaymentService
                     $interestAmount = $result['remaining_interest'];
                     $interest_amount += $result['interest_amount'];
                     $principal_amount += $result['principal_amount'];
-                    dd($result);
                 }
             }
+            dd($result);
             if ($amount > 0) {
 
                 $this->handleRemainingAmount($contract, $amount, $cash, $payments->last()->id, $deal_id,$date);
