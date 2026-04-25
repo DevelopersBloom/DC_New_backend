0bf5531786259c70115c0dc170080ba9de2aa779
AnMashinyan <ani.mashinyan00@gmail.com>
2026-04-25 16:22:12 +0400

    Add backup

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -21,7 +21,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('backup:clean')->dailyAt('01:00');
-        $schedule->command('backup:run')->dailyAt('16:13');
+        $schedule->command('backup:run')->dailyAt('16:23');
 
 
         $schedule->job(new UpdateClientClassificationsNew)
