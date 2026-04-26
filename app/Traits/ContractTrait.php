1645a001a73087be8c0af21e6674ebbd03ab5772
AnMashinyan <ani.mashinyan00@gmail.com>
2026-04-26 23:00:41 +0400

    bugfix calc curreny payment

--- app/Traits/ContractTrait.php
+++ app/Traits/ContractTrait.php
@@ -510,6 +510,7 @@ trait ContractTrait
                 break;
             }
         }
+        dd($balance,$interestAmount,$daysIntoCurrentPeriod);
 
         $journalId = DocumentJournal::where('journalable_id', $contract->id)
             ->where('journalable_type', 'App\Models\Contract')
