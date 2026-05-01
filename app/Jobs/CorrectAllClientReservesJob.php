64f743027e2c41fccd50135e6d86b8210303fa87
AnMashinyan <ani.mashinyan00@gmail.com>
2026-05-01 18:04:18 +0400

    Fix v03

--- app/Jobs/CorrectAllClientReservesJob.php
+++ app/Jobs/CorrectAllClientReservesJob.php
@@ -68,7 +68,7 @@ class CorrectAllClientReservesJob implements ShouldQueue
                         ->orderBy('date','desc')
                         ->first();
                         if ($client->id == 6) {
-                            dd($client->id,$clientClassification->classification->name);
+                            dd($dateStr,$client->id,$clientClassification->classification->name);
                         }
                     $firstContract = $client->contracts()
                         ->where('status', 'initial')
