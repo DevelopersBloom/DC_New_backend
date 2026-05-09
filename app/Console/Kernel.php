d73466c65b529a0fd177a673629e78b7801ea806
AnMashinyan <ani.mashinyan00@gmail.com>
2026-05-09 20:00:34 +0400

    Fix name property

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -31,7 +31,7 @@ class Kernel extends ConsoleKernel
             ->appendOutputTo(storage_path('logs/schedule.log'));
 
         $schedule->job(new ProcessContractDailyRate)
-            ->dailyAt('00:05')
+            ->dailyAt('20:01')
             ->timezone('Asia/Yerevan')
             ->withoutOverlapping(10)
             ->appendOutputTo(storage_path('logs/schedule.log'));
