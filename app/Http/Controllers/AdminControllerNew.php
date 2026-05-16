8a2bad6bb41a45f4eca1f02b8504ceea0db75ac5
AnMashinyan <ani.mashinyan00@gmail.com>
2026-05-16 20:41:23 +0400

    Show principal [ayment in deals

--- app/Http/Controllers/AdminControllerNew.php
+++ app/Http/Controllers/AdminControllerNew.php
@@ -611,7 +611,7 @@ class AdminControllerNew extends Controller
             'penalty',
             'discount',
             'created_by',
-            DB::raw('(amount - COALESCE(interest_amount, 0) - COALESCE(penalty, 0)) as principal_payment')
+            DB::raw("(CASE WHEN purpose IN ('Ամբողջական վճարում', 'Հերթական վճարում', 'Մասնակի վճարում') THEN amount - COALESCE(interest_amount, 0) - COALESCE(penalty, 0) ELSE NULL END) as principal_payment")
         )
             ->with('client:id,name,surname')
             ->with('contract:id,mother,num')
