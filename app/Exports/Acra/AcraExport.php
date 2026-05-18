c81755b069251024dacf0d376589480943cb010c
AnMashinyan <ani.mashinyan00@gmail.com>
2026-05-18 21:58:15 +0400

    Fix acra export

--- app/Exports/Acra/AcraExport.php
+++ app/Exports/Acra/AcraExport.php
@@ -338,8 +338,8 @@ class AcraExport
             if ($firstOverdue && ($overdueMother > 0 || $overdueInterest > 0)) {
                 $overdueDate = Carbon::parse($firstOverdue->date)->addDay();
                 $this->setDateCellValue($sheet, 'M' . $row, $overdueDate);
-                $days = Carbon::parse($firstOverdue->date)->diffInDays(Carbon::parse($this->to));
-                $sheet->setCellValue('W' . $row, $days);
+                $days = Carbon::parse($firstOverdue->date)->diffInDays(Carbon::parse($this->from));
+                $sheet->setCellValue('W' . $row, $days-1);
             } else {
                 $sheet->setCellValue('W' . $row, 0);
             }
