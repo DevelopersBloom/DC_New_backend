47f1376c83a4b447cd44a7a1a09cdb4e767fda7d
AnMashinyan <ani.mashinyan00@gmail.com>
2026-05-18 23:32:15 +0400

    Add notifications

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -31,7 +31,7 @@ class Kernel extends ConsoleKernel
             ->appendOutputTo(storage_path('logs/schedule.log'));
 
         $schedule->job(new ProcessContractDailyRate)
-            ->dailyAt('00:05')
+            ->dailyAt('11:34')
             ->timezone('Asia/Yerevan')
             ->withoutOverlapping(10)
             ->appendOutputTo(storage_path('logs/schedule.log'));
