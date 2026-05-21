9b8031a7eadeff7cc2d73bddff9634ea4cffa021
AnMashinyan <ani.mashinyan00@gmail.com>
2026-05-21 23:21:58 +0400

    Fix payment

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -245,6 +245,7 @@ class PaymentService
                 $interestPayment -= $paidInterest;
                 $payment->interest_payment  -= $paidInterest;
 
+                dd($payment->to_date >= $date, $payment->to_date,$date,  $payment->interest_payment ,$interestPayment);
                 if ($payment->to_date >= $date) {
                     $paidPrincipal = min($amount - $paidInterest, $principalPayment);
                     $remainingAmount = 0;
