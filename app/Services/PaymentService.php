24992f9fcee45685ddb5524fa7f591688ba5608e
AnMashinyan <ani.mashinyan00@gmail.com>
2026-05-24 16:51:37 +0400

    Principal over payment type bug test

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -254,7 +254,7 @@ class PaymentService
                 $payment->principal_payment -= $paidPrincipal;
                 $contract->left             = max(0, $contract->left - $paidPrincipal);
                 $contract->provided_amount  = max(0, $contract->provided_amount - $paidPrincipal);
-
+dd($amount,$paidInterest,$paidPrincipal);
                 if ($payment->principal_payment > 0 || $payment->interest_payment > 0 ) {
                     $this->partiallyCompletePayment($payment, $paidInterest+$paidPrincipal, $deal_id, [], $principalPayment, $interestPayment);
                 } else {
