c4fa0d731343d16309de1f118a4829f634ebac35
AnMashinyan <ani.mashinyan00@gmail.com>
2026-05-24 18:10:41 +0400

    Principal over payment type bug test

--- app/Http/Controllers/PaymentControllerNew.php
+++ app/Http/Controllers/PaymentControllerNew.php
@@ -156,7 +156,6 @@ class PaymentControllerNew extends Controller
             $class    = $contract->client->classification->name;
             $clientId = $contract->client_id;
             $docNum   = Transaction::getNextDocumentNumber();
-dd($result);
             $interest            = $result['interest_amount'];
             $principal           = $result['principal_amount'];
             $prepaymentPrincipal = $result['prepayment_principal'] ?? 0;
