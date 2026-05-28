016b6105147975c2e77926c3f72682d7b3f59659
AnMashinyan <ani.mashinyan00@gmail.com>
2026-05-28 22:20:29 +0400

    Fix loss payment

--- app/Http/Controllers/PaymentControllerNew.php
+++ app/Http/Controllers/PaymentControllerNew.php
@@ -163,6 +163,7 @@ class PaymentControllerNew extends Controller
 
             // ---- Interest ----
             if ($interest > 0) {
+                dd(5);
                 $rule = $this->getPostingRule($this->resolveEvent('pay_interest_amount', $class, $cash));
 dd($rule,$cash);
                 $journalInterest = $this->postEntry(
