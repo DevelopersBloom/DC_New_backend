16a88e27b9fb1d11833e3596c35519530925e761
AnMashinyan <ani.mashinyan00@gmail.com>
2026-05-30 12:20:58 +0400

    Fix schedule of process daily rate

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -31,7 +31,7 @@ class Kernel extends ConsoleKernel
             ->appendOutputTo(storage_path('logs/schedule.log'));
 
         $schedule->job(new ProcessContractDailyRate)
-            ->dailyAt('11:36')
+            ->dailyAt('00:05')
             ->timezone('Asia/Yerevan')
             ->withoutOverlapping(10)
             ->appendOutputTo(storage_path('logs/schedule.log'));
