83f0fb747bd530e8d5f4f5a969e99e602bb9c607
AnMashinyan <ani.mashinyan00@gmail.com>
2026-06-19 20:40:02 +0400

    last payment interest recalculation

--- app/Services/Payments/LatePaymentInterestRecalculator.php
+++ app/Services/Payments/LatePaymentInterestRecalculator.php
@@ -44,7 +44,6 @@ class LatePaymentInterestRecalculator
             $dailyRate = (float) $contract->interest_rate / 100.0;
 
             $updates = $this->computeUpdates($allPayments, $originalBalance, $dailyRate, $paymentDate, $historicalEntries);
-dd($updates);
             foreach ($updates as ['payment' => $payment, 'interest' => $interest]) {
                 $payment->interest_payment = round($interest, 2);
                 $payment->amount           = round((float) $payment->principal_payment + $interest, 2);
