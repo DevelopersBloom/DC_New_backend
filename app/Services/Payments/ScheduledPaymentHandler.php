c835ba1b333106a42d98ddd5d86c238e9beab327
AnMashinyan <ani.mashinyan00@gmail.com>
2026-07-15 17:39:40 +0400

    Fix payemtn

--- app/Services/Payments/ScheduledPaymentHandler.php
+++ app/Services/Payments/ScheduledPaymentHandler.php
@@ -342,7 +342,6 @@ class ScheduledPaymentHandler
             ? Carbon::parse($paymentDate)->setTimezone('Asia/Yerevan')->startOfDay()
             : Carbon::now('Asia/Yerevan')->startOfDay();
         $elapsedDays = max(0, $from->diffInDays($now));
-dd($elapsedDays);
         $balance = (float) $contract->provided_amount;
         $rate    = (float) $contract->interest_rate;
 
