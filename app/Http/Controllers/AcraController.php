342d122db26593cff199e8c252c2dffb8bedc4b3
AnMashinyan <ani.mashinyan00@gmail.com>
2026-07-15 13:04:59 +0400

    Fix acra

--- app/Http/Controllers/AcraController.php
+++ app/Http/Controllers/AcraController.php
@@ -94,7 +94,7 @@ class AcraController
         $updatedClientIds = Client::whereBetween('updated_at', [$from, $to])->pluck('id')->toArray();
         $contractClientIds = $contracts->pluck('client_id')->toArray();
         $allClientIds = array_unique(array_merge($contractClientIds, $updatedClientIds));
-        $allClients = Client::whereIn('id', $allClientIds)->get();
+        $allClients = Client::whereIn('id', $allClientIds)->where('name','!=','Գրիշա')->get();
 
         $acraExport = new AcraExport($contracts, $allClients, $from, $to);
         $fileData = $acraExport->export();
