693ac91321e4da073cfb44a8c106540f2f126ed7
AnMashinyan <ani.mashinyan00@gmail.com>
2026-07-16 11:58:26 +0400

    FIx due date payment

--- app/Http/Controllers/PaymentControllerNew.php
+++ app/Http/Controllers/PaymentControllerNew.php
@@ -152,8 +152,7 @@ class PaymentControllerNew extends Controller
                 $history->$field = $result[$field] ?? 0;
                 $deal->$field    = $result[$field] ?? 0;
             }
-            dd($result);
-            $deal->principal_amount = $result['principal_amount'] ?? 0;
+            $deal->principal_amount = $result['principal_amount'] + $result['partial_amount'] ?? 0;
             $deal->prepayment       = $result['prepayment_principal'] ?? 0;
 
             $history->save();
