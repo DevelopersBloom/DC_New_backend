4ed69d87ee2c1c0dc161468bd1c5219ba3dc626e
AnMashinyan <ani.mashinyan00@gmail.com>
2026-07-22 12:59:20 +0400

    test payment

--- app/Traits/ContractTrait.php
+++ app/Traits/ContractTrait.php
@@ -564,7 +564,7 @@ trait ContractTrait
                 break;
             }
         }
-
+dd($interestAmount);
         $journalId = DocumentJournal::where('journalable_id', $contract->id)
             ->where('journalable_type', 'App\Models\Contract')
             ->value('id');
