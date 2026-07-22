b519ba2045d8baec47acef96fc879c1f17603a7b
AnMashinyan <ani.mashinyan00@gmail.com>
2026-07-22 11:20:48 +0400

    Fix due date

--- app/Http/Controllers/PaymentControllerNew.php
+++ app/Http/Controllers/PaymentControllerNew.php
@@ -383,6 +383,7 @@ class PaymentControllerNew extends Controller
 
     public function previewPayment(Request $request): JsonResponse
     {
+        dd($request->all());
         $request->validate([
             'contract_id'       => 'required|exists:contracts,id',
             'amount'            => 'required|numeric|min:0.01',
