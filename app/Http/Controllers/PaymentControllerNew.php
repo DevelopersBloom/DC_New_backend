86c30ca0b8dfa1f6c3877a50ba9e0c99e9ce8c5b
AnMashinyan <ani.mashinyan00@gmail.com>
2026-07-26 22:33:18 +0400

    test full payment

--- app/Http/Controllers/PaymentControllerNew.php
+++ app/Http/Controllers/PaymentControllerNew.php
@@ -471,6 +471,7 @@ class PaymentControllerNew extends Controller
 
             $deal = $this->createDeal($totalAmount, null, null, null, null, 'in', $contract->id, $contract->client->id, $newOrder->id, $cash, null, Contract::FULL_PAYMENT, 'full_payment', $history->id, null, null, null, $date);
             $oldPaymentAmount = $this->calcPaidAmount($contract);
+           dd($oldPaymentAmount);
             $fullPaymentResult = $this->paymentService->processFullPayment($contract, $totalAmount, $payer, $cash, $deal->id,$date);
             $paymentId = $fullPaymentResult['payment_id'];
             $prepaymentRefundAmount = $fullPaymentResult['refund_amount'];
