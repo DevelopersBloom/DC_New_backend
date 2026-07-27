08f64efd83b41d274392ede2321c1ab0a04ebfb6
AnMashinyan <ani.mashinyan00@gmail.com>
2026-07-27 22:07:52 +0400

    Fix ndm

--- routes/api.php
+++ routes/api.php
@@ -175,7 +175,7 @@ Route::group(['middleware' => 'jwt.auth'], function () {
 
         Route::get('loan-ndm/interest', [LoanNdmController::class, 'calculateInterest'])->middleware('can:calculate_loan_interest');
         Route::post('loan-ndm/post-interest',[LoanNdmController::class,'postInterest'])->middleware('can:post_loan_interest');
-        Route::post('loan-ndm/repay',[LoanNdmController::class, 'repay'])->middleware(['can:repay_loan_interest', 'idempotent:loan_ndm.repay']);
+        Route::post('loan-ndm/repay',[LoanNdmController::class, 'repay'])->middleware(['can:repay_loan_interest']);// 'idempotent:loan_ndm.repay'
         Route::get('/loan-ndm/remaining/{id}', [ChartOfAccountController::class, 'remainingAmount'])->middleware('can:view_remaining_loan');
         Route::get('/loan-ndm/by-journal/{journal}', [LoanNdmController::class, 'get'])->middleware('can:view_loan_by_journal');
         Route::get('/transactions', [TransactionController::class, 'index'])->middleware('can:view_transactions');
