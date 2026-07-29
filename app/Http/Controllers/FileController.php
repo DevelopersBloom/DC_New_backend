4fd435236ceb3ce922d5a162107c9c9d7c3b3026
AnMashinyan <ani.mashinyan00@gmail.com>
2026-07-29 21:55:26 +0400

    fix contract bond downalod

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -430,7 +430,7 @@ class FileController extends Controller
             'effective_annual_rate' => $effectiveRate . ' %',
             'deadline' => $deadline,
             'bank_name' => $client->bank_name,
-            'account_number' => $client->account_number,
+            'account_card_number' => $client->account_number,
             'card_number' => $client->card_number,
             'user_name' => $userName,
             'lump_rate' => $lumpRate,
