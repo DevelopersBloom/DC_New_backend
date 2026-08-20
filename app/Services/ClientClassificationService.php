a73869612bfe8fb8e9e925df6767e2d8a9ee4175
AnMashinyan <ani.mashinyan00@gmail.com>
2026-08-20 11:05:41 +0400

    fix acc import

--- app/Services/ClientClassificationService.php
+++ app/Services/ClientClassificationService.php
@@ -65,10 +65,7 @@ class ClientClassificationService
         try {
             $currentOverdueDays = $this->maxOverdueDaysForClient($client);
             $currentClassification = $this->classificationByOverdue($currentOverdueDays);
-            if ($client->id == 55) {
 
-                dd($client->name,$currentOverdueDays,$currentClassification->order ,$classification->order);
-            }
             if (($client->classification_id === $classification->id) || ($currentClassification->order > $classification->order)) {
                 DB::commit();
                 return false;
