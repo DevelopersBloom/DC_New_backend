a5f57b5f0f1a9dbee31e2ef48b1888e3b5de5ee3
AnMashinyan <ani.mashinyan00@gmail.com>
2026-09-02 19:56:46 +0400

    fix order download

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -1109,7 +1109,7 @@ class FileController extends Controller
             'date' => isset($order->date) ? $this->formatArmenianDate($order->date) : null,
             'receiver' => $order->receiver ?? null,
             'contract_id' => $contract?->num,
-            'num' => $order->num ?? $contract?->num ?? null,
+            'num' =>  $contract?->num ?? null,
             'client' => $client ? ($client->type === 'legal' ? $client->company_name : $client->name . ' ' . $client->surname . ' ' . ($client->middle_name ?? '')) : null,
             'cl_dob' => $client?->date_of_birth
                 ? Carbon::parse($client->date_of_birth)->format('d.m.Y')
